f994816260c3c604a79e58e24bbf64d818a5da57
Thomas Cort <linuxgeek@gmail.com>
2010-03-18 19:48:43 -0400

    Add disclaimer

--- www/index.php
+++ www/index.php
@@ -188,6 +188,7 @@ header('Content-type: text/html; charset=utf-8');
   <div id="map"></div>
   <div id="sidebar">
     <div id="sidetxt">
+      <p><em>Disclaimer</em>: The points of interest you see on this page are for testing purposes only and may be totally inaccurate. This website is not affiliated with any government agency.</p>
       <p>Copyright &copy; 2010 <a href="http://www.tomcort.com/">Thomas Cort</a><br/><small>This application is <a href="http://www.gnu.org/philosophy/free-sw.html">Free Software</a>. Get the source code <a href="http://github.com/tcort/odopoi">here</a>.</small></p>
       <p><small>Maps are licensed <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-By-SA 2.0</a> by <a href="http://www.openstreetmap.org/">OpenStreetMap</a>. Map display is licensed <a href="http://svn.openlayers.org/trunk/openlayers/license.txt">BSD-style</a> by <a href="http://openlayers.org/">OpenLayers</a>. Marker icons are licensed <a href="http://creativecommons.org/licenses/by-sa/3.0/">CC-By-SA 3.0</a> by <a href="http://code.google.com/p/google-maps-icons/">Maps icons collection</a>.</small></p>
     </div>
