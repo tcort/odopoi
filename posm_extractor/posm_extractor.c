b3c10d1d649c5ac9b49fb648cd4c051bb58133bb
Thomas Cort <linuxgeek@gmail.com>
2010-03-28 12:12:05 -0400

    All of Canada.

--- posm_extractor/posm_extractor.c
+++ posm_extractor/posm_extractor.c
@@ -45,11 +45,12 @@
 #include <expat.h>
 
 #define BUFSIZE (4194304)
-#define LAT_MIN ( 45.00)
-#define LAT_MAX ( 47.00)
-#define LON_MIN (-77.00)
-#define LON_MAX (-74.00)
+#define LAT_MIN ( 41.50)
+#define LAT_MAX ( 83.25)
+#define LON_MIN (-141.25)
+#define LON_MAX (-47.50)
 
+/* for ottawa/gatineau: lat 45 to 47 | lon -77 to -74 */
 /* for all of Canada: lat 41.50 to 83.25 | lon -141.25 to -47.50 */
 
 typedef struct tag {
